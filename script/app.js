7c3d2e2774e489a16265734892ca4b2280a23aa2
omarbabou <obabou50@gmail.com>
2022-05-31 14:45:47 +0200

    First interaction

---
+++ script/app.js
@@ -0,0 +1,14 @@
+const body = document.querySelector('body');
+const navigation = document.querySelector('.icons');
+
+// --------------------MOBILE-MENU--------------------------
+
+navigation.addEventListener('click', () => {
+  body.classList.toggle('open-menu');
+});
+
+const menuLink = document.querySelectorAll('.ul-link');
+
+menuLink.forEach((e) => e.addEventListener('click', () => {
+  body.classList.remove('open-menu');
+}));

ac3fd5230283b13a038e0098f8f3df0c4ff72271
omarbabou <obabou50@gmail.com>
2022-06-03 08:35:36 +0200

    Add images to the menu section

--- script/app.js
+++ script/app.js
@@ -1,5 +1,6 @@
 const body = document.querySelector('body');
 const navigation = document.querySelector('.icons');
+const menuService = document.querySelector('.menu-services');
 
 // --------------------MOBILE-MENU--------------------------
 
@@ -14,3 +15,94 @@ menuLink.forEach((e) => e.addEventListener('click', () => {
 }));
 
 // --------------------------  MENU SECTION -----------------------
+
+const capstone = [
+  {
+    id: 1,
+    image: '/graphics/user-1.jpg',
+    name: 'Salt Bae',
+    role: 'A smart assistant to make driving more safe.',
+    descript:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking.',
+  },
+  {
+    id: 2,
+    image: '/graphics/user-2.jpg',
+    name: 'Salt Bae',
+    role: 'A smart assistant to make driving more safe.',
+    descript:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking.',
+  },
+  {
+    id: 3,
+    image: '/graphics/user-3.jpg',
+    name: 'Salt Bae',
+    role: 'A smart assistant to make driving more safe.',
+    descript:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking.',
+  },
+  {
+    id: 4,
+    image: '/graphics/user-2.jpg',
+    name: 'Salt Bae',
+    role: 'A smart assistant to make driving more safe.',
+    descript:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking.',
+  },
+  {
+    id: 5,
+    image: '/graphics/user-1.jpg',
+    name: 'Salt Bae',
+    role: 'A smart assistant to make driving more safe.',
+    descript:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking.',
+  },
+  {
+    id: 6,
+    image: '/graphics/user-3.jpg',
+    name: 'Salt Bae',
+    role: 'A smart assistant to make driving more safe.',
+    descript:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking.',
+  },
+];
+
+for (let i = 0; i < capstone.length; i += 1) {
+  const taste = capstone[i];
+
+  menuService.innerHTML += `
+  <div class="user-card" id="user-${taste.id}">
+   <div class="user">
+     <img src="${taste.image}" alt="" class="user-img" width="55%">
+   </div> 
+     <div class="user-info">
+        <h4>${taste.name}</h4>
+        <h5>${taste.role}</h5>
+        <p>${taste.descript}</p>
+     </div>
+  </div>  
+    
+    `;
+}
+
+/* const button = document.querySelector('.show-more');
+const menu = document.querySelectorAll('.user-card');
+let state = true;
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (state) {
+    menu.forEach((team) => {
+      team.classList.add('show-more');
+      button.innerHTML = 'See less';
+    });
+    state = false;
+  } else {
+    state = false;
+    menu.forEach((team) => {
+      team.classList.remove('show-more');
+      button.innerHTML = 'Show more';
+    });
+    state = true;
+  }
+}); */
